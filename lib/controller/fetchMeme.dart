fb5f5126ccdee7e7976484c49671d82129a0493a
Aman Kumar <37516866+ak3255@users.noreply.github.com>
2024-07-03 00:41:39 +0530

    Update fetchMeme.dart

--- lib/controller/fetchMeme.dart
+++ lib/controller/fetchMeme.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 
 class FetchMeme {
   static fetchNewMeme() async {
-    Response response = await get(Uri.parse("https://meme-api.com/gimme"));
+    Response response = await get(Uri.parse("https://meme-api.com/gimme/dank"));
 
     Map body_data = jsonDecode(response.body);
     return body_data["url"];
